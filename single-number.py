d3c4d152e0fc3a299f9db0d668bb8bbcef21ab32
Robel Roba <robel.roba@aastustudent.edu.et>
2026-02-09 16:55:39 +0300

    Create single-number.py

---
+++ single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        c = Counter(nums)
+        
+        for i in c:
+            if c[i] == 1:
+                return i
+        
