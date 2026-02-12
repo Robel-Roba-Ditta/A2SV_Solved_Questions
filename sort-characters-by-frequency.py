ecad7aeb72c540bd3a32de1c8219b2ab805e90b4
Robel Roba <robel.roba@aastustudent.edu.et>
2026-02-12 14:32:38 +0300

    Create sort-characters-by-frequency.py

---
+++ sort-characters-by-frequency.py
@@ -0,0 +1,10 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        l = list(s)
+        o = []
+        c = Counter(l)
+
+        for i, j in sorted(c.items(), key=lambda x: x[1], reverse=True):
+            o.append(i * j)  
+
+        return "".join(o)
