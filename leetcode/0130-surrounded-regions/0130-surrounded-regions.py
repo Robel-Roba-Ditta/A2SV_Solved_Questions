711e0bc5ecc04c28cb36c6ef1a2a2d3903a83b63
Robel Roba <robel.roba@aastustudent.edu.et>
2026-04-27 19:07:35 +0300

    Add solution for Surrounded Regions

---
+++ leetcode/0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows, cols = len(board), len(board[0])
+        flag = set()
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= rows or c >= cols or 
+                board[r][c] != 'O' or (r, c) in flag):
+                return
+            flag.add((r, c))
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if ((r == 0 or c == 0 or r == rows - 1 or c == cols - 1) and 
+                    board[r][c] == 'O'):
+                    dfs(r, c)
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O' and (r, c) not in flag:
+                    board[r][c] = 'X'
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
